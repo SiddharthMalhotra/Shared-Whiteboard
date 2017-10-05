3b6dab318b12581b3dafbc58d53471ad325e086c
SiddharthMalhotra <codemalhotra@gmail.com>
2017-10-05 15:38:39 +1100

    Add files via upload

--- PaintApplication/src/whiteboardtools/Tool.java
+++ PaintApplication/src/whiteboardtools/Tool.java
@@ -4,4 +4,4 @@ package whiteboardtools;
 public enum Tool {PENCIL  , ERASER  , BRUSH  ,
                   SQUARE  , OVAL    , LINE   ,
                   POLYGON , AIRBRUSH, PICKER , 
-                  BUCKET  , RECTANGLE, FILLER, ROUND_RECT };
+                  BUCKET  , RECTANGLE, FILLER, ROUND_RECT, WRITER };
